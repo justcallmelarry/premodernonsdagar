53f669714e28f718514a6dc2bf3b8ceb5f858edb
Lauri Vuorela <lauri.vuorela@gmail.com>
2025-10-10 22:57:46 +0200

    use normal picc instead for size concerns

--- scripts/card_db.py
+++ scripts/card_db.py
@@ -74,7 +74,7 @@ def main() -> None:
 
         db_card = {
             "name": name,
-            "image_url": card.get("image_uris", {}).get("png", ""),
+            "image_url": card.get("image_uris", {}).get("normal", ""),
             "legality": legality,
             "card_type": card_type,
         }
